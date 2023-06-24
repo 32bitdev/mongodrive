cbb5ef5732b80521ba71619650ba5641da7305c6
32bitdev <32bitdev@gmail.com>
2023-06-25 00:12:08 +0530

    Frontend API Routes Added

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -1,3 +1,6 @@
 export const host = "http://localhost:8000";
 export const registerRoute = `${host}/auth/register`;
 export const loginRoute = `${host}/auth/login`;
+export const logoutRoute = `${host}/auth/logout`;
+export const uploadRoute = `${host}/file/upload`;
+export const getFilesRoute = `${host}/file/getFiles`;
