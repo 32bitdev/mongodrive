4b8b7e4f24f25fa718d36b0fdfb483a7822eb33f
32bitdev <32bitdev@gmail.com>
2023-06-24 13:10:31 +0530

    MongoDB Collections Updated/Added

--- server/collections/mongoCollections.js
+++ server/collections/mongoCollections.js
@@ -1,9 +1,11 @@
 //mongodb collections configuration
 require("dotenv").config();
 const mongodb = require("mongodb");
-const { MongoClient } = require("mongodb");
+const { ObjectId, MongoClient } = require("mongodb");
 const client = new MongoClient(process.env.MONGO_URL);
 const db = client.db("mongodrive");
 client.connect();
 const Users = db.collection("users");
-module.exports = { Users };
+const FileDetails= db.collection("fileDetails");
+const FileBucket = new mongodb.GridFSBucket(db, { bucketName: "drive" });
+module.exports = { Users, FileDetails, FileBucket, ObjectId };
