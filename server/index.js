4c5571af52d30f7b2de272fc9199b0720ce7fc3f
32bitdev <32bitdev@gmail.com>
2023-06-24 13:06:35 +0530

    Server Modules and Req/Res Headers Updated

--- server/index.js
+++ server/index.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const userRoutes = require("./routes/userRoutes");
+const fileUpload = require("express-fileupload");
 const app = express();
 require("dotenv").config();
 
 app.use(express.json());
+app.use(fileUpload());
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.setHeader("Access-Control-Expose-Headers", "Content-Disposition");
+  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Authorization, authorization, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+  next();
+});
 app.use("/auth", userRoutes);
 const server = app.listen(process.env.PORT, () =>
   console.log(`Server started on port ${process.env.PORT}`)
