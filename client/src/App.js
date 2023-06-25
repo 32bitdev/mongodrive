9259acf3322d58c88e57314e4da1c16b57ee5885
ArchanaY1203 <ay1840450@gmail.com>
2023-06-26 00:25:58 +0530

    App.js Routes Imported

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Stream from "./pages/Stream";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/stream" element={<Stream />} />
       </Routes>
     </BrowserRouter >
   )
