e3af1acd7b5449922e04761c468900a36cba59bd
ArchanaY1203 <ay1840450@gmail.com>
2023-06-26 00:56:48 +0530

    Server File Routes Added

--- server/routes/fileRoutes.js
+++ server/routes/fileRoutes.js
@@ -1,6 +1,11 @@
 //server routes to handle get and post requests
-const { upload, getFiles } = require("../controllers/fileControllers");
+const { upload, download, deleteFile, getFiles, getVideos, streamAction, stream } = require("../controllers/fileControllers");
 const router = require("express").Router();
 router.post("/upload", upload);
 router.post("/getFiles", getFiles);
+router.post("/getVideos", getVideos);
+router.post("/download", download);
+router.post("/delete", deleteFile);
+router.get("/stream/:streamId", stream);
+router.post("/streamAction", streamAction);
 module.exports = router;
